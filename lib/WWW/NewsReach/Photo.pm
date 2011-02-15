b79f50bfd223aa6524052616ddd0cd13f65c3d3a
Adam Taylor <adamjctaylor@gmail.com>
2011-02-15 23:20:54 +0000

    Add Photo class

---
+++ lib/WWW/NewsReach/Photo.pm
@@ -0,0 +1,52 @@
+=head1 NAME
+
+=cut
+
+package WWW::NewsReach::Photo;
+
+use strict;
+use warnings;
+
+use Moose;
+
+use URI;
+
+has $_ => (
+    is => 'ro',
+    isa => 'Int',
+) for qw( width height );
+
+has type => (
+    is => 'ro',
+    isa => 'Str',
+); 
+
+has url => (
+    is => 'ro',
+    isa => 'URI',
+);
+
+=head1 METHODS
+
+=head2 WWW::NewsReach::Photo->new_from_xml
+
+=cut
+
+sub new_from_xml {
+    my $class = shift;
+    my ( $xml ) = @_;
+
+    my $self = {};
+
+    foreach (qw[ width height ]) {
+        $self->{$_} = $xml->findnodes("//$_")->[0]->textContent;
+    }
+
+    $self->{type} = $xml->findnodes("//type")->[0]->textContent;
+
+    $self->{url} = URI->new( $xml->findnodes("//url")->[0]->textContent );
+
+    return $class->new( $self );
+}
+
+1;
